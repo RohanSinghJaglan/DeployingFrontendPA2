2000e2c38f4cb51db1d7456922e20950c3fa402b
Rohan Singh Jaglan <rohan.singh@kalvium.community>
2025-02-14 12:10:50 +0530

    first commit

---
+++ src/app.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Rohan Singh Jaglan</h1>
+      <p><b>Contact:</b>rohansinghjaglan@gmail.com</p>
+      <p><b>Phone:</b> +9180000479XXX</p>
+    </div>
+  );
+}
+
+export default App;
